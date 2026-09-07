8eadce7f837077b8f529af9d2592e08f27314076
Hossain Nur Shakib <hossainnurshakib@gmail.com>
2026-09-07 17:42:13 +0600

    fix: suppress hydration warning on body element

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default async function RootLayout({
           dangerouslySetInnerHTML={{ __html: safeJsonLd(webSiteJsonLd) }}
         />
       </head>
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <a
           href="#main-content"
           className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[100] focus:bg-dark-bg focus:text-dark-text focus:px-4 focus:py-2 focus:rounded-lg focus:outline-none focus:ring-2 focus:ring-accent"
